c3828f6fd5ae94874c48398481f932a09fa321da
Lebedev Alexander <winden999@gmail.com>
2018-07-19 20:01:16 +0300

    some changes for productFactory

--- api/database/factories/ProductFactory.php
+++ api/database/factories/ProductFactory.php
@@ -10,14 +10,21 @@ $factory->define(Product::class, function (Faker $faker) {
         ['name' => 'weight', 'value' => $faker->randomNumber()],
         ['name' => 'color', 'value' => $faker->randomElement(['yellow', 'black', 'white', 'red', 'green', 'orange'])],
     ];
+    $images = [
+        "https://pp.userapi.com/c631329/v631329421/5a6d0/X7yAATDvmik.jpg",
+        "https://pp.userapi.com/c606823/v606823421/1aad/8Jrr2sDrNP0.jpg",
+        "https://sun1-15.userapi.com/c543109/v543109365/31795/VzglO7Ek4Zc.jpg",
+        "https://sun1-16.userapi.com/c7006/v7006851/20fc9/ACW4fTXr86I.jpg",
+    ];
 
     return [
-        'name' => 'Товар №' . $faker->randomNumber(),
+        'name' => $faker->name,
         'code' => 'CODE_' . $faker->randomNumber(),
         'description' => $faker->paragraph,
         'price' => $faker->randomFloat(3, 100, 1000000),
         'specifications' => json_encode($specs),
         'tags' => json_encode($tags),
         'category_id' => function() { return Category::inRandomOrder()->first()->id; },
+        'img_path' => $faker->randomElement($images),
     ];
 });
