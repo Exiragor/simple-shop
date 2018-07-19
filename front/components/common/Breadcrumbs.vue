2a2945f3cebb59b32c7078e4a3273940621cba42
Lebedev Alexander <winden999@gmail.com>
2018-07-19 20:39:59 +0300

    add breadcrumbs

---
+++ front/components/common/Breadcrumbs.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+  .breadcrumbs
+    span
+      router-link.breadcrumbs__item(to="/") Главная
+      |  / #{' '}
+    span(v-for="(route, index) in routes" :key="index")
+      router-link.breadcrumbs__item(:to="route.link") {{ route.name }}
+      | / #{' '}
+    span.breadcrumbs__item {{ activeRoute }}
+</template>
+
+<script>
+  export default {
+    name: 'Breadcrumbs',
+    props: {
+      routes: {
+        type: Array,
+        default: () => [],
+      },
+      activeRoute: {
+        type: String,
+        required: true,
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import "~assets/scss/vars";
+  // colors
+  $black: map_get($colors, black);
+
+  .breadcrumbs__item {
+    color: $black;
+    &:hover {
+      color: black;
+    }
+  }
+</style>
--- front/pages/products/_id.vue
+++ front/pages/products/_id.vue
@@ -1,15 +1,17 @@
 <template lang="pug">
   .container
-    h1.text-center.mb20 Catalog
+    breadcrumbs.mb35(:active-route="product.name")
     product-detail(:product="product")
 </template>
 
 <script>
   import ProductDetail from '~/components/product/Detail.vue'
+  import Breadcrumbs from '~/components/common/Breadcrumbs.vue'
   export default {
     name: 'Product',
     components: {
-      ProductDetail
+      ProductDetail,
+      Breadcrumbs
     },
     async fetch ({ store, params }) {
       await store.dispatch("products/loadProduct", { productID: params.id })
