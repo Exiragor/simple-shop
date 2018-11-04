067a98b8f8b0418be1243f1941b6d0ebf72384b9
Lebedev Alexander <winden999@gmail.com>
2018-11-04 14:27:28 +0300

    result sum in cart

--- front/pages/cart/index.vue
+++ front/pages/cart/index.vue
@@ -1,12 +1,17 @@
-<template lang="pug">
-  section.container.main.pt30
-    h1.text-center.mb35 Cart
-    product(v-for="product in products" :key="product.id" :product="product")
+<template>
+  <section class="container main pt30">
+    <h1 class="text-center mb35"> Cart</h1>
+    <product v-for="product in products" :key="product.id" :product="product" />
+    <div class="result mt30">
+      <span>Result: ${{ getSum() }}</span>
+    </div>
+  </section>
 </template>
 
 <script>
   import { mapState } from 'vuex'
   import Product from '@/components/cart/Product'
+  import { formatPrice } from '~/helpers'
 
   export default {
     name: 'CartIndex',
@@ -19,5 +24,24 @@
         counts: state => state.cart.counts || []
       })
     },
+    methods: {
+      getSum() {
+        let sum = 0
+        for (let index in this.products) {
+          sum += this.products[index].price * this.counts[index]
+        }
+        return formatPrice(sum)
+      }
+    }
   }
 </script>
+
+<style lang="scss" scoped>
+  .result {
+    display: flex;
+    font-size: 30px;
+    span {
+      margin-left: auto;
+    }
+  }
+</style>
