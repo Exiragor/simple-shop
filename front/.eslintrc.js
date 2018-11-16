f95fbddc6295d2f69a97c8985d1a19e97e62d339
Lebedev Alexander <winden999@gmail.com>
2018-11-16 15:09:51 +0300

    add some rules for eslint

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -17,5 +17,17 @@ module.exports = {
     'vue'
   ],
   // add your custom rules here
-  rules: {}
+  rules: {
+    'vue/max-attributes-per-line': 0,
+    'vue/order-in-components': 0,
+    'vue/attributes-order': 0,
+    // less restricted v-for -> v-if rules
+    'vue/no-confusing-v-for-v-if': 0,
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  }
 }
