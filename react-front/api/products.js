23c155f97850c074819852c9c53f3407cc82a516
Alexander Lebedev <exiragor@yandex.ru>
2019-05-23 12:09:00 +0300

    add calls to api

---
+++ react-front/api/products.js
@@ -0,0 +1,17 @@
+import req from './request';
+
+let mainUrl = '/products';
+
+export function loadProducts(count, page, query = false) {
+  let queryStr = '';
+  if (query) {
+    for (let param in query) {
+      queryStr += `&${param}=${query[param]}`;
+    }
+  }
+  return req.get(mainUrl + `?count=${count}&page=${page}` + queryStr);
+}
+
+export function loadProduct(id) {
+  return req.get(mainUrl + `/${id}`);
+}
