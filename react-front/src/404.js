530205e4ea16333b1a7691538391f7fb6044a7ff
Alexander Lebedev <exiragor@yandex.ru>
2019-05-27 00:58:04 +0300

    404

---
+++ react-front/src/404.js
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const page404 = () => <h1 className="text-center">404. Nothing founded!</h1>;
--- react-front/src/App.js
+++ react-front/src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { HeaderTop } from './components/header/HeaderTop';
 import { HomePage } from './pages/index';
 import { ProductDetailPage } from './pages/productDetail';
+import { page404 } from './404'
 
 function App(app) {
   return (
-    <div className="App">
-      <HeaderTop />
-      <Router>
-        <Route exact path={['/', '/page/:page']} component={HomePage} />
-        <Route path="/product/:id" component={ProductDetailPage} />
-      </Router>
-    </div>
+    <Router>
+      <div className="App">
+        <HeaderTop />
+        <Switch>
+          <Route exact path={['/', '/page/:page']} component={HomePage} />
+          <Route path="/product/:id" component={ProductDetailPage} />
+          <Route component={page404} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
