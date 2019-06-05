5a82873dcb8d770b73ca38fb250a4263c6a4d7fa
Alexander Lebedev <exiragor@yandex.ru>
2019-06-05 13:35:08 +0300

    nothing found in products list

--- react-front/src/components/product/List.js
+++ react-front/src/components/product/List.js
@@ -4,7 +4,8 @@ import { ProductsType } from './types';
 
 export const ProductsList = ({ products }) => (
     <div className="products-list row">
-        {products.map(product => <div className="col-md-3 mb-4" key={product.id}><ProductCard product={product} /></div>)}
+        {products && products.map(product => <div className="col-md-3 mb-4" key={product.id}><ProductCard product={product} /></div>)}
+        {!products.length && <h3 className="w-100 text-center">Nothing found.</h3>}
     </div>
 );
 
